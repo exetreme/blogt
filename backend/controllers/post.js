19a26730aa5f822e5738377b5645e5ab78fd4b33
Tung <tokyoghoulge@gmail.com>
2022-06-08 12:50:14 +0700

    featured

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -1,21 +1,34 @@
 const Post = require("../models/post");
+const FeaturedPost = require("../models/featuredPost");
+
+const FEATURED_POST_COUNT = 4;
+
+const addFeaturedPost = async (postID) => {
+  const featuredPost = new FeaturedPost({ post: postID });
+  await featuredPost.save();
+
+  const featuredPosts = await FeaturedPost.find({}).sort({ createdAt: -1 });
+
+  featuredPosts.forEach(async (post, index) => {
+    if (index >= FEATURED_POST_COUNT)
+      await FeaturedPost.findByIdAndDelete(post._id);
+  });
+};
 
 exports.createPost = async (req, res) => {
-  try {
-    const { title, meta, content, slug, author, tags } = req.body;
-    const newPost = new Post({
-      title,
-      meta,
-      content,
-      slug,
-      author,
-      tags,
-    });
+  const { title, meta, content, slug, author, tags, featured } = req.body;
+  const newPost = new Post({
+    title,
+    meta,
+    content,
+    slug,
+    author,
+    tags,
+  });
 
-    await newPost.save();
+  await newPost.save();
 
-    res.json(newPost);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
+  if (featured) await addFeaturedPost(newPost._id);
+
+  res.json(newPost);
 };
